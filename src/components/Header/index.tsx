d59c0245ac9a49839ca71152385aedb63ba5c7ba
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2021-04-24 14:00:35 -0300

    Finalization of the player's features - shuffle, reapeat, next, previous and slider

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
@@ -10,8 +11,12 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr" />
-
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Podcastr" />
+        </a>
+      </Link>
+      
       <p>O melhor para você ouvir, sempre</p>
       <span>{currentDate}</span>
     </header>
